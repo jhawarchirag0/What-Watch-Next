9bf640048709d1c8af637fbca2c27134dc3b8492
Chirag Jhawar <chirag.jhavar@somaiya.edu>
2021-04-02 20:33:49 +0530

    Added Homepage

---
+++ js-files/slideshow.js
@@ -0,0 +1,51 @@
+var slideIndex = 0;
+var x;
+
+function init(){
+  var i;
+  for(i=0; i<x.length; i++){
+    if(i!=slideIndex){
+      x[i].style.display = "none";
+    }
+    else{
+      x[i].style.display = "block"
+    }
+  }
+}
+
+window.onload = function() {
+  x = document.getElementsByClassName("movie");
+  init();
+}
+
+function nextImage(){
+  slideIndex++;
+  if( slideIndex>=x.length ){
+    slideIndex = 0
+  }
+  var i;
+  for(i=0; i<x.length; i++){
+    if(i!=slideIndex){
+      x[i].style.display = "none";
+    }
+    else{
+      x[i].style.display = "block"
+    }
+  }
+}
+
+function prevImage(){
+  slideIndex--;
+  if( slideIndex<0 ){
+    slideIndex = x.length - 1;
+  }
+  var i;
+  for(i=0; i<x.length; i++){
+    if(i!=slideIndex){
+      x[i].style.display = "none";
+    }
+    else{
+      x[i].style.display = "block"
+    }
+  }
+}
