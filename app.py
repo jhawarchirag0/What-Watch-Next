c093d91f50c9b75af9ac63e45e05df7aa755a350
Manas Thakker` <manas.thakker@somaiya.edu>
2021-04-07 12:51:28 +0530

    added cast section

--- app.py
+++ app.py
@@ -20,6 +20,9 @@ def search():
     # return the data from the API 
     # data is like images, information about the movie, etc.
     movie_name = request.form["movie_name"]
+
+
+    
     tmdb.API_KEY = "4d437864b2f333cb0fc07c9b104397c6"
     concat_link = "http://api.themoviedb.org/3/search/movie?query="+movie_name+"&api_key=4d437864b2f333cb0fc07c9b104397c6&language=en-US"
     info = requests.get(concat_link)
@@ -82,43 +85,32 @@ def search():
 
     #Actors_info:
 
-    # actor1_name = actors[0][0]
-    # actor1_character = actors[0][1]
-    # actor1_image = actors[0][2]
-    # actor1_bdate = actors[0][4]
-    # actor1_birthplace = actors[0][5]
-    # actor1_biography = actors[0][6]
-
-    # actor2_name = actors[1][0]
-    # actor2_character = actors[1][1]
-    # actor2_image = actors[1][2]
-    # actor2_bdate = actors[1][4]
-    # actor2_birthplace = actors[1][5]
-    # actor2_biography = actors[1][6]
-
-    # actor3_name = actors[2][0]
-    # actor3_character = actors[2][1]
-    # actor3_image = actors[2][2]   
-    # actor3_bdate = actors[2][4]
-    # actor3_birthplace = actors[2][5]
-    # actor3_biography = actors[2][6]
+    actor1_name = actors[0][0]
+    actor1_character = actors[0][1]
+    actor1_image = actors[0][2]
+    
+    actor2_name = actors[1][0]
+    actor2_character = actors[1][1]
+    actor2_image = actors[1][2]
+   
+    actor3_name = actors[2][0]
+    actor3_character = actors[2][1]
+    actor3_image = actors[2][2]   
+   
 
     #director_info:
 
     director_name = director[0]
     director_character = "Director"
     director_image = director[1]
-    director_bdate = director[3]
-    director_birthplace = director[4]
-    director_biography = director[5]
                 
     movie = [movie_title,movie_overview,movie_rating,movie_release_date,movie_genres,movie_image]
-    director = [director_name,director_character,director_image,director_bdate,director_birthplace,director_biography]
-    # actor1 = [actor1_name,actor1_character,actor1_image,actor1_bdate,actor1_birthplace,actor1_biography]
-    # actor2 = [actor2_name,actor2_character,actor2_image,actor2_bdate,actor2_birthplace,actor2_biography]
-    # actor3 = [actor3_name,actor3_character,actor3_image,actor3_bdate,actor3_birthplace,actor3_biography]
+    director = [director_name,director_character,director_image]
+    actor1 = [actor1_name,actor1_character,actor1_image]
+    actor2 = [actor2_name,actor2_character,actor2_image]
+    actor3 = [actor3_name,actor3_character,actor3_image]
     
-    return render_template('search-results.html',movien=movie)
+    return render_template('search-results.html',movien=[movie,director,actor1,actor2,actor3,director])
 
 
 if __name__=="__main__":
