31e8c5addc3420d79ec89c053420d41e36e554b3
Chirag Jhawar <chirag.jhavar@somaiya.edu>
2021-04-15 22:51:10 +0530

    Generated requirements.txt

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template,json
 import os
-import tmdbsimple as tmdb
-import requests
+import numpy as np
 import pandas as pd
 from numpy import load
+from flask import Flask, request, jsonify, render_template,json
+import tmdbsimple as tmdb
+import requests
+from tmdbv3api import TMDb, Movie
 
 app=Flask(__name__)
 new_df,indices,new_df2 = None,None,None
 cosine_sim_overview,cosine_sim_cast,cosine_sim_crew,cosine_sim_genres=None,None,None,None
 actor1,actor2,actor3,actor4,director=None,None,None,None,None
 
+
+@app.before_first_request
+def before_first_request_func():
+    file = 'dataset/tmdb_processed.csv'
+    df2 = pd.read_csv(file)
+    df2 = df2.fillna("")
+    global new_df,indices,new_df2
+    new_df2 = df2.iloc[:25000]
+    new_df = new_df2.iloc[:10][["title", "overview", "vote_average", "poster_path"]].to_dict("list")
+    new_df = json.dumps(new_df)
+    indices = pd.DataFrame()
+    indices["index"] = new_df2.index
+    indices["title"] = new_df2['title']
+    # load numpy array from npy file
+    # global cosine_sim_overview,cosine_sim_cast,cosine_sim_crew,cosine_sim_genres
+    # cosine_sim_overview = load(r'/wieghts_MRS/data_cosine_sim_overview.npy')
+    # cosine_sim_cast = load(r'/wieghts_MRS/data_cosine_sim_cast.npy')
+    # cosine_sim_crew = load(r'/wieghts_MRS/data_cosine_sim_crew_2.npy')
+    # cosine_sim_genres = load(r'/wieghts_MRS/data_cosine_sim_genres.npy')
+
+
+
 @app.route('/')
 def home():
-    from tmdbv3api import TMDb
-    from tmdbv3api import Movie
-
     tmdb = TMDb()
     tmdb.api_key = '4d437864b2f333cb0fc07c9b104397c6'
     tmdb.language = 'en'
     tmdb.debug = True
 
-   
     movie = Movie()
     popularr = movie.popular()
     popular=popularr[:7]
@@ -36,9 +55,6 @@ def home():
         l.append('https://image.tmdb.org/t/p/original//'+m.poster_path)
         final_popular[i]=l
 
-
-    #for new releases
-    import requests
     url="https://api.themoviedb.org/3/movie/upcoming?api_key=4d437864b2f333cb0fc07c9b104397c6&language=en-US&page=1"
     
     info = requests.get(url)
@@ -62,36 +78,11 @@ def home():
     return render_template('index1.html',final_popular=final_popular,new_release=new_release,json_details=json_details)
 
 
-@app.before_first_request
-def before_first_request_func():
-    file = 'dataset/tmdb_processed.csv'
-    df2 = pd.read_csv(file)
-    df2 = df2.fillna("")
-    global new_df,indices,new_df2
-    new_df2 = df2.iloc[:25000]
-    new_df = new_df2.iloc[:10][["title", "overview", "vote_average", "poster_path"]].to_dict("list")
-    new_df = json.dumps(new_df)
-    indices = pd.Series(new_df2.index, index=new_df2['title']).drop_duplicates()
-    # load numpy array from npy file
-    # global cosine_sim_overview,cosine_sim_cast,cosine_sim_crew,cosine_sim_genres
-    # cosine_sim_overview = load(r'/wieghts_MRS/data_cosine_sim_overview.npy')
-    # cosine_sim_cast = load(r'/wieghts_MRS/data_cosine_sim_cast.npy')
-    # cosine_sim_crew = load(r'/wieghts_MRS/data_cosine_sim_crew_2.npy')
-    # cosine_sim_genres = load(r'/wieghts_MRS/data_cosine_sim_genres.npy')
-
-
-
 @app.route('/search',methods=['POST'])
 def search():
 
-    # return the data from the API 
-    # data is like images, information about the movie, etc.
-    movie_name = request.form["movie_name"]
-    # get_recom = get_recommendations(movie_name).to_dict("list")
-    # get_recom = json.dumps(get_recom)
-    get_recom = new_df
-
     global actor1,actor2,actor3,actor4,director
+    movie_name = request.form["movie_name"]
 
     tmdb.API_KEY = "4d437864b2f333cb0fc07c9b104397c6"
     concat_link = "http://api.themoviedb.org/3/search/movie?query="+movie_name+"&api_key=4d437864b2f333cb0fc07c9b104397c6&language=en-US"
@@ -100,13 +91,9 @@ def search():
         print("Results not fetched")
     info = info.json()
     search_df = pd.DataFrame(info['results'])
-    # search_df.sort_values(by=['vote_count', 'vote_average'],ascending=False, inplace=True)
-    # print(search_df[['title', 'vote_count', 'vote_average']])
     search_df = search_df.to_dict(orient='records')[0]
     id2 = search_df.copy()
     search_df['backdrop_path'] = "https://image.tmdb.org/t/p/original" + search_df['backdrop_path']
-    # print("Selected :")
-    # print(search_df)
     movie_id = id2['id']
 
     gen = requests.get('https://api.themoviedb.org/3/genre/movie/list?api_key=4d437864b2f333cb0fc07c9b104397c6&language=en-US')
@@ -122,6 +109,11 @@ def search():
     movie = tmdb.Movies(movie_id)
     response = movie.info()
     dict1 = {'title': id2['original_title'],'overview': id2['overview'],'rating': id2['vote_average'],'release date': id2['release_date']}
+
+    # get_recom = get_recommendations(dict1['title']).to_dict("list")
+    # get_recom = json.dumps(get_recom)
+    get_recom = new_df
+
     cast = movie.credits()['cast']
     cast_df = pd.DataFrame(cast)
     cast_df = cast_df[cast_df['known_for_department']=='Acting']
@@ -270,18 +262,17 @@ def search():
 def cast1():
     return render_template('cast_info.html')
 
-# def get_recommendations(title):
-#     idx = indices[title]
-#     final_sim = np.array(cosine_sim_overview[idx])
-#     final_sim += np.array(cosine_sim_cast[idx])
-#     final_sim += np.array(cosine_sim_crew[idx])
-#     final_sim += np.array(cosine_sim_genres[idx])
-#     sim_scores = list(enumerate(final_sim))
-#     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-#     # print(sim_scores)
-#     sim_scores = sim_scores[1:11]
-#     movie_indices = [i[0] for i in sim_scores]
-#     return new_df2[['title','overview','vote_average']].iloc[movie_indices]
+def get_recommendations(title):
+    idx = indices[indices["title"] == title]["index"].to_list()[0]
+    final_sim = np.array(cosine_sim_overview[idx])
+    final_sim += np.array(cosine_sim_cast[idx])
+    final_sim += np.array(cosine_sim_crew[idx])
+    final_sim += np.array(cosine_sim_genres[idx])
+    sim_scores = list(enumerate(final_sim))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11]
+    movie_indices = [i[0] for i in sim_scores]
+    return new_df2['title'].iloc[movie_indices]
 
     
 if __name__=="__main__":
